539aa2ea9b951396ee2751f9ea34a1d84d0258b0
Ichi Kanaya <kanaya@pineapple.cc>
2023-05-16 07:57:59 +0900

    Coding.

--- dawkins/src/Lib.hs
+++ dawkins/src/Lib.hs
@@ -2,5 +2,20 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Bits
+import System.Random
+
+evaluate :: Word -> Int
+evaluate x = popCount x
+
+color :: Word -> (Word, Int)
+color x = (x, evaluate x)
+
+eval :: (Word, Int) -> Int
+eval (x, e) = e
+
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ take 10 (randomRs (0, 10) newStdGen)
+
